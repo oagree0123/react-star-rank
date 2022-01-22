0ecafd917ac7d23819d6c1df75e3a206282c21b6
Oh YeJun <oagree0123@naver.com>
2022-01-22 21:36:00 +0900

    💄 Add Rating components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+import Rating from './Rating';
 
 function App() {
+  const [dayList, setDayList] = useState([
+    "월", "화", "수", "목", "금", "토", "일"
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap className="App">
+      <Container>
+      <MainTitle>내 일주일은?</MainTitle>
+      {
+        dayList.map((day, index) => {
+          return (
+            <Rating key={index} day={day} />
+          );
+        })
+      }
+      </Container>
+    </AppWrap>
   );
 }
+
+const AppWrap = styled.div`
+  text-align: center;
+`;
+
+const MainTitle = styled.h3`
+  
+`;
+
+const Container = styled.div`
+  margin: 5vh auto;
+  padding: 5vh 0;
+  width: 80vw;
+  height: 90vh;
+  max-width: 350px;
+  background-color: #fff;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+`;
 
 export default App;
---
+++ src/Rating.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rating = (props) => {
+  let days = props.day;
+  let rank =  Math.floor(Math.random(0) * 5) + 1;
+
+  console.log(days, rank)
+  return (
+    <RankWrap>
+      <DayTitle>{days}</DayTitle>
+      {
+        [...Array(5)].map((star, index) => {
+          console.log(rank)
+          return(
+            (index < rank) ?
+            <RankCircle 
+              className={rank}
+              key={index} 
+              style={{backgroundColor: "rgb(255, 235, 59)"}}
+            /> :
+            <RankCircle className={rank} key={index} />
+          );
+        })
+      }
+      <TriButton />
+    </RankWrap>
+  );
+};
+
+const RankWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DayTitle = styled.p`
+  margin-right: 0.5rem;
+  font-weight: bold;
+`;
+
+const RankCircle = styled.div`
+  margin: 5px;
+  width: 30px;
+  height: 30px;
+  background-color: rgb(221, 221, 221);
+  border-radius: 30px;
+`
+
+const NoRankCircle = styled.div`
+  margin: 5px;
+  width: 30px;
+  height: 30px;
+  background-color: rgb(221, 221, 221);
+  border-radius: 30px;
+`
+
+const TriButton = styled.div`
+  appearance: none;
+  background-color: transparent;
+  border-color: transparent purple;
+  width: 0px;
+  height: 0px;
+  border-top-width: 1rem;
+  border-top-style: solid;
+  border-bottom-width: 1rem;
+  border-bottom-style: solid;
+  border-left-width: 1.6rem;
+  border-left-style: solid;
+  color: rgb(255, 255, 255);
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default Rating;
