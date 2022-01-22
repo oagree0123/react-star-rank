23d42306facc6f52c851ff7268bddf0ebabd38cc
Oh YeJun <oagree0123@naver.com>
2022-01-22 23:44:00 +0900

    💄 Change Container styled-component

--- src/App.js
+++ src/App.js
@@ -8,30 +8,16 @@ import Review from './Review';
 function App() {
   return (
     <AppWrap className="App">
-      <Container>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/review/:click_day" element={<Review />} />
       </Routes>
-      </Container>
     </AppWrap>
   );
 }
 
 const AppWrap = styled.div`
   text-align: center;
-`;
-
-const Container = styled.div`
-  margin: 5vh auto;
-  padding: 5vh 0;
-  width: 80vw;
-  height: 90vh;
-  max-width: 350px;
-  background-color: #fff;
-  border-radius: 5px;
-  border: 1px solid #ddd;
-  box-sizing: border-box;
 `;
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import Rating from "./Rating";
 
 const Home = () => {
-  const [dayList, setDayList] = useState([
+  const dayList = [
     "월",
     "화",
     "수",
@@ -14,18 +14,30 @@ const Home = () => {
     "금",
     "토",
     "일",
-  ]);
+  ];
 
   return (
-    <>
+    <Container>
       <MainTitle>내 일주일은?</MainTitle>
       {dayList.map((day, index) => {
         return <Rating key={index} day={day} />;
       })}
-    </>
+    </Container>
   );
 };
 
 const MainTitle = styled.h3``;
+
+const Container = styled.div`
+  margin: 5vh auto;
+  padding: 5vh 0;
+  width: 80vw;
+  height: 90vh;
+  max-width: 350px;
+  background-color: #fff;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+`;
 
 export default Home;
